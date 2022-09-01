dd41ab14c77e82e19f9eb198f1f8ba06539b7e99
nandhu002 <112645933+nandhu002@users.noreply.github.com>
2022-09-01 21:44:40 +0530

    Update prime number.py

--- prime number.py
+++ prime number.py
@@ -3,5 +3,7 @@ for num in range(1,200):
     for i in range(2,(num//2+1)):
         if (num % i == 0):
             count = count+1
+            break
+            
     if (count == 0 and num != 1):
         print("%d" %num, end= ' ')
